7513f3c6ec90047c18e0746e598e4e6ec1740c2f
Andy Cross <across@dextersystems.com>
2024-12-01 07:27:13 -0500

    part 2 cleanup

--- src/main/kotlin/aoc2024/Day01.kt
+++ src/main/kotlin/aoc2024/Day01.kt
@@ -12,44 +12,20 @@ fun main() {
         return left.indices.sumOf { abs(left[it] - right[it]) }
     }
 
-    fun part2(input: List<String>): Long {
-        val left = mutableListOf<Int>()
-        val right = mutableMapOf<Int, Int>()
+    fun part2(input: List<String>): Int {
+        val cols = input.filter { it.isNotEmpty() }.map { it.split("\\s+".toRegex()) }
 
-        input.forEach { line ->
-            if (line.isNotEmpty()) {
-                val (a, b) = line.split("\\s+".toRegex())
-                left.add(a.toInt())
+        val left = cols.map { it[0].toInt() }
+        val right = cols.map { it[1].toInt() }
 
-                if (right.containsKey(b.toInt())) {
-                    right[b.toInt()] = right[b.toInt()]!!.plus(1)
-                } else {
-                    right[b.toInt()] = 1
-                }
-            }
-        }
-
-        var result: Long = 0
-
-        left.forEach { it ->
-            if (right.containsKey(it)) {
-                val rightValue = right[it]!!
-
-//                println("key $it appears $rightValue times on right")
-                result += (it.toLong() * rightValue.toLong())
-            }
-        }
-
-        return result
+        return left.sumOf { l -> l * right.count { it == l } }
     }
 
-    // Or read a large test input from the `src/main/resources/Day01_test.txt` file:
     val testInput = readAsLines("Day01_test")
     check(part1(testInput) == 11)
 
-    // Read the input from the `src/main/resources/Day01.txt` file.
     val input = readAsLines("Day01")
-    part1(input).println() // 1223326 correct
-    part2(input).println() // 21070419 correct
+    part1(input).println()
+    part2(input).println()
 }
 
