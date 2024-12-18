c24f1dac40fcca8da73514fe7e198b4cc7db2f63
Andy Cross <across@dextersystems.com>
2024-12-18 11:20:22 -0500

    D18 tidyup

--- src/main/kotlin/aoc2024/Day18.kt
+++ src/main/kotlin/aoc2024/Day18.kt
@@ -21,42 +21,32 @@ class Day18 {
             )
         }
 
-        fun inBounds(gridSize: Int): Boolean {
-            return (x >= 0 && y >= 0 && x < gridSize && y < gridSize)
-        }
-
-        override fun toString(): String {
-            return "$x,$y"
-        }
+        fun inBounds(gridSize: Int) = (x >= 0 && y >= 0 && x < gridSize && y < gridSize)
+        override fun toString() = "$x,$y"
     }
 
-    data class State(val pos: Vec2, val visited: List<Vec2>) {}
-
-    // bring in the heavy guns of dijkstra for this one
-    data class Node(val pos: Vec2)
-
-    data class Graph<Node>(
-        val vertices: Set<Node>,
-        val edges: Map<Node, Set<Node>>,
-        val weights: Map<Pair<Node, Node>, Int>
+    data class Graph<Vec2>(
+        val vertices: Set<Vec2>,
+        val edges: Map<Vec2, Set<Vec2>>,
+        val weights: Map<Pair<Vec2, Vec2>, Int>
     )
 }
 
-private fun dijkstra(graph: Graph<Node>, start: Node): Map<Node, Node?> {
-    val S: MutableSet<Node> = mutableSetOf()
+private fun dijkstra(graph: Graph<Vec2>, start: Vec2): Map<Vec2, Vec2?> {
+    val set: MutableSet<Vec2> = mutableSetOf()
 
     val delta = graph.vertices.associateWith { Int.MAX_VALUE }.toMutableMap()
     delta[start] = 0
 
-    val previous: MutableMap<Node, Node?> = graph.vertices.associateWith { null }.toMutableMap()
+    val previous: MutableMap<Vec2, Vec2?> = graph.vertices.associateWith { null }.toMutableMap()
 
-    while (S != graph.vertices) {
-        val v: Node = delta
-            .filter { !S.contains(it.key) }
+    while (set != graph.vertices) {
+        val v: Vec2 = delta
+            .filter { !set.contains(it.key) }
             .minBy { it.value }
             .key
 
-        graph.edges.getValue(v).minus(S).forEach { neighbor ->
+        graph.edges.getValue(v).minus(set).forEach { neighbor ->
             val newPath = delta.getValue(v) + graph.weights.getValue(Pair(v, neighbor))
 
             if (newPath < delta.getValue(neighbor)) {
@@ -65,14 +55,14 @@ private fun dijkstra(graph: Graph<Node>, start: Node): Map<Node, Node?> {
             }
         }
 
-        S.add(v)
+        set.add(v)
     }
 
     return previous.toMap()
 }
 
-fun <Node> shortestPath(shortestPathTree: Map<Node, Node?>, start: Node, end: Node): List<Node> {
-    fun pathTo(start: Node, end: Node): List<Node> {
+fun <T> shortestPath(shortestPathTree: Map<T, T?>, start: T, end: T): List<T> {
+    fun pathTo(start: T, end: T): List<T> {
         if (shortestPathTree[end] == null) return listOf(end)
         return listOf(pathTo(start, shortestPathTree[end]!!), listOf(end)).flatten()
     }
@@ -80,20 +70,16 @@ fun <Node> shortestPath(shortestPathTree: Map<Node, Node?>, start: Node, end: No
     return pathTo(start, end)
 }
 
-fun getRouting(nodeMap: Map<Vec2, Node>, bytes: List<Vec2>, gridSize: Int): Routing<Node, Node?> {
-    val nodes = nodeMap.values
-
-    // for each node, get the set of other nodes (also free spaces) it connects to
+fun getRouting(nodes: List<Vec2>, bytes: List<Vec2>, gridSize: Int): Routing<Vec2, Vec2?> {
     val edges = buildMap {
         nodes.forEach { node ->
             put(
                 node,
-                node.pos.next().filter { it.inBounds(gridSize) && it !in bytes }.mapNotNull { nodeMap[it] }.toSet()
+                node.next().filter { it.inBounds(gridSize) && it !in bytes }.map { it }.toSet()
             )
         }
     }
 
-    // need to map each Pair<from, to> to a weight
     val weights = buildMap {
         edges.map { (nodeFrom, toSet) ->
             toSet.map { nodeTo ->
@@ -102,55 +88,34 @@ fun getRouting(nodeMap: Map<Vec2, Node>, bytes: List<Vec2>, gridSize: Int): Rout
         }
     }
 
-    return listOf(nodeMap[Vec2(0, 0)]!!).map { it to dijkstra(Graph(nodes.toSet(), edges, weights), it) }
+    return listOf(Vec2(0, 0)).map { it to dijkstra(Graph(nodes.toSet(), edges, weights), it) }
 }
 
-private fun buildNodeMap(gridSize: Int, bytes: List<Vec2>): Map<Vec2, Node> {
-    return buildMap {
-        (0..<gridSize).forEach { y ->
-            (0..<gridSize).forEach { x ->
-                val pos = Vec2(x, y)
-
-                if (pos !in bytes) {
-                    put(pos, Node(pos))
-                }
-            }
-        }
-    }
+private fun buildNodeList(gridSize: Int, bytes: List<Vec2>): List<Vec2> {
+    return (0..<gridSize).flatMap { y -> (0..<gridSize).map { x -> Vec2(x, y) } }.filter { it !in bytes }
 }
 
-fun routeTo(origin: Node, destination: Node, routing: Routing<Node, Node?>): List<Node> {
+fun routeTo(origin: Vec2, destination: Vec2, routing: Routing<Vec2, Vec2?>): List<Vec2> {
     return shortestPath(routing.first { it.first == origin }.second, origin, destination)
 }
 
-fun shortestRoute(nodeMap: Map<Vec2, Node>, bytes: List<Vec2>, start: Vec2, finish: Vec2, gridSize: Int): List<Node> {
-    val routing = getRouting(nodeMap, bytes, gridSize)
-
-    val startNode = nodeMap.getValue(start)
-    val endNode = nodeMap.getValue(finish)
-
-    return routeTo(startNode, endNode, routing)
+fun shortestRoute(nodeMap: List<Vec2>, bytes: List<Vec2>, start: Vec2, finish: Vec2, gridSize: Int): List<Vec2> {
+    return routeTo(start, finish, getRouting(nodeMap, bytes, gridSize))
 }
 
 fun minSteps(bytes: List<Vec2>, start: Vec2, finish: Vec2, gridSize: Int): Int {
-    return shortestRoute(buildNodeMap(gridSize, bytes), bytes, start, finish, gridSize).size - 1
+    return shortestRoute(buildNodeList(gridSize, bytes), bytes, start, finish, gridSize).size - 1
 }
 
 fun simulate(bytes: List<Vec2>, seconds: Int = bytes.size, gridSize: Int): Int {
-    val fallenBytes = bytes.take(seconds)
-
-//    drawGrid(gridSize, fallenBytes)
-
-    // now it's a simple traversal puzzle like the other day
-    return minSteps(fallenBytes, Vec2(0, 0), Vec2(gridSize - 1, gridSize - 1), gridSize)
+    return minSteps(bytes.take(seconds), Vec2(0, 0), Vec2(gridSize - 1, gridSize - 1), gridSize)
 }
 
 fun simulatePart2(bytes: List<Vec2>, gridSize: Int): String {
     val start = Vec2(0, 0)
     val finish = Vec2(gridSize - 1, gridSize - 1)
 
-    // possible value is between (minOk+1) and gridSize
-    // need to do a binary search between them
+    // do binary search to find where the end stops being reachable
     var lastOk = 0
     var firstNotOk = bytes.size
 
@@ -169,20 +134,9 @@ fun simulatePart2(bytes: List<Vec2>, gridSize: Int): String {
     return bytes[firstNotOk - 1].toString()
 }
 
-private fun canReach(
-    bytes: List<Vec2>,
-    sec: Int,
-    gridSize: Int,
-    start: Vec2,
-    finish: Vec2
-): Boolean {
-    val fallen = bytes.take(sec)
-    val nodeMap = buildNodeMap(gridSize, fallen)
-
-    val route = shortestRoute(nodeMap, bytes.take(sec), start, finish, gridSize).map { it.pos }
-    val ok = (start in route && finish in route)
-
-    return ok
+private fun canReach(bytes: List<Vec2>, sec: Int, gridSize: Int, start: Vec2, finish: Vec2): Boolean {
+    val route = shortestRoute(buildNodeList(gridSize, bytes.take(sec)), bytes.take(sec), start, finish, gridSize)
+    return (start in route && finish in route)
 }
 
 
