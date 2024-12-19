9e2b21b8673342b99b05bc42e4e9e6c6fe8808fc
Andy Cross <across@dextersystems.com>
2024-12-19 17:30:28 -0500

    D19 add timing and rearrange code

--- src/main/kotlin/aoc2024/Day19.kt
+++ src/main/kotlin/aoc2024/Day19.kt
@@ -1,10 +1,62 @@
 package aoc2024
 
 import java.math.BigInteger
-
-val cache = mutableMapOf<String, List<String>>()
+import kotlin.time.measureTime
 
 fun main() {
+    val cache = mutableMapOf<String, List<String>>()
+
+    fun String.combinations(towels: List<String>): MutableMap<Pair<Int, Int>, BigInteger> {
+        val queue = ArrayDeque<String>()
+        queue.add(this)
+
+        val longestTowel = towels.maxOf { it.length }
+
+        val towelCombos = mutableMapOf<Pair<Int, Int>, MutableSet<String>>()
+
+        while (queue.isNotEmpty()) {
+            val design = queue.removeFirst()
+
+            val possibleTowels =
+                cache.getOrPut(design.take(longestTowel)) { towels.filter { t -> design.startsWith(t) } }
+
+            possibleTowels.forEach { towel ->
+                val remaining = design.drop(towel.length)
+
+                val fromTo = Pair(design.length, remaining.length)
+                towelCombos[fromTo] = towelCombos.getOrDefault(fromTo, mutableSetOf()).apply { add(towel) }
+
+                if (remaining !in queue && remaining.isNotEmpty()) {
+                    queue.add(remaining)
+                }
+            }
+        }
+
+        return buildMap {
+            towelCombos.forEach { (key, value) -> put(key, BigInteger.valueOf(value.size.toLong())) }
+        }.toMutableMap()
+    }
+
+    tailrec fun waysToReach(combinations: MutableMap<Pair<Int, Int>, BigInteger>): BigInteger {
+        if (combinations.isEmpty()) return BigInteger.ZERO
+
+        val highestValue = combinations.keys.maxOf { it.second }
+
+        if (highestValue == 0) {
+            return combinations.map { it.value }.sumOf { it }
+        } else {
+            val entriesToRemove = combinations.filterKeys { it.second == highestValue }
+            val entriesToMultiply = combinations.filterKeys { it.first == highestValue }
+
+            val multiplier = entriesToRemove.map { it.value }.sumOf { it }
+
+            entriesToRemove.keys.forEach { combinations.remove(it) }
+            entriesToMultiply.keys.forEach { combinations[it] = combinations[it]!! * multiplier }
+
+            return waysToReach(combinations)
+        }
+    }
+
     fun towels(input: List<String>) = input.first().split(", ")
     fun designs(input: List<String>) = input.last().lines()
 
@@ -39,54 +91,7 @@ fun main() {
 
     val input = readAsBlocks("Day19")
 
-    part1(input).println() // 360
-    part2(input).println() // 577474410989846
-}
-
-private fun String.combinations(towels: List<String>): MutableMap<Pair<Int, Int>, BigInteger> {
-    val queue = ArrayDeque<String>()
-    queue.add(this)
-
-    val longestTowel = towels.maxOf { it.length }
-
-    val towelCombos = mutableMapOf<Pair<Int, Int>, MutableSet<String>>()
-
-    while (queue.isNotEmpty()) {
-        val design = queue.removeFirst()
-
-        val possibleTowels = cache.getOrPut(design.take(longestTowel)) { towels.filter { t -> design.startsWith(t) } }
-
-        possibleTowels.forEach { towel ->
-            val remaining = design.drop(towel.length)
-
-            val fromTo = Pair(design.length, remaining.length)
-            towelCombos[fromTo] = towelCombos.getOrDefault(fromTo, mutableSetOf()).apply { add(towel) }
-
-            if (remaining !in queue && remaining.isNotEmpty()) queue.add(remaining)
-        }
-    }
-
-    return buildMap {
-        towelCombos.forEach { (key, value) -> put(key, BigInteger.valueOf(value.size.toLong())) }
-    }.toMutableMap()
-}
-
-tailrec fun waysToReach(combinations: MutableMap<Pair<Int, Int>, BigInteger>): BigInteger {
-    if (combinations.isEmpty()) return BigInteger.ZERO
-
-    val highestValue = combinations.keys.maxOf { it.second }
-
-    if (highestValue == 0) {
-        return combinations.map { it.value }.sumOf { it }
-    } else {
-        val entriesToRemove = combinations.filterKeys { it.second == highestValue }
-        val entriesToMultiply = combinations.filter { it.key.first == highestValue }
-
-        val multiplier = entriesToRemove.map { it.value }.sumOf { it }
-
-        entriesToRemove.keys.forEach { combinations.remove(it) }
-        entriesToMultiply.keys.forEach { combinations[it] = combinations[it]!! * multiplier }
-
-        return waysToReach(combinations)
-    }
+    // timings are in the 150-250ms range
+    measureTime { part1(input).println() }.also { it.println() }
+    measureTime { part2(input).println() }.also { it.println() }
 }
