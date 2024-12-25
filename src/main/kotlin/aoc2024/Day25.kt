7228ff4239b6d0c062e872a48b35ab6db57c66bb
Andy Cross <across@dextersystems.com>
2024-12-25 08:17:55 -0500

    Day 25 tidyup

--- src/main/kotlin/aoc2024/Day25.kt
+++ src/main/kotlin/aoc2024/Day25.kt
@@ -7,40 +7,23 @@ fun main() {
 
         blocks.forEach { block ->
             val lines = block.lines()
-
-            val maxCol = lines.first().length - 1
+            val cols = 0..<lines.first().length
 
             if (lines.first().all { it == '#' }) {
-                val heights = (0..maxCol).map { col ->
-                     lines.drop(1).count { l -> l[col] == '#' }
-                }
-
-                locks.add(heights)
+                cols.map { col -> lines.drop(1).count { l -> l[col] == '#' } }.apply { locks.add(this) }
             } else {
-                val heights = (0..maxCol).map { col ->
-                    lines.dropLast(1).count { l -> l[col] == '#' }
-                }
-
-                keys.add(heights)
+                cols.map { col -> lines.dropLast(1).count { l -> l[col] == '#' } }.apply { keys.add(this) }
             }
         }
 
         return Pair(locks, keys)
     }
 
-    fun overlaps(lock: List<Int>, key: List<Int>): Boolean {
-        return lock.indices.any { lock[it] + key[it] > 5 }
-    }
+    fun overlaps(lock: List<Int>, key: List<Int>) = lock.indices.any { lock[it] + key[it] > 5 }
 
     fun part1(input: List<String>): Int {
         val (locks, keys) = parse(input)
-
-        val pairs = locks.flatMap { l ->
-                keys.filterNot { k -> overlaps(l, k) }
-                    .map { k -> Pair(l, k) }
-        }
-
-        return pairs.count()
+        return locks.flatMap { l -> keys.filterNot { k -> overlaps(l, k) }.map { k -> Pair(l, k) } }.count()
     }
 
     val testInput = readAsBlocks("Day25_test")
@@ -49,4 +32,3 @@ fun main() {
     val input = readAsBlocks("Day25")
     part1(input).println()
 }
-
